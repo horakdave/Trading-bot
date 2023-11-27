abecd5f9edb4ffd0ed463f3ea455427ec779fc66
horakdave <horakdejv@gmail.com>
2023-11-27 17:51:24 +0100

    1.2 update + graphing tool
    I made a new graphing tool that takes historical stocks data and tells you when it would buy/sell and short term average + long term average

--- Project_TNP/TNP-1.2.py
+++ Project_TNP/TNP-1.2.py
@@ -11,7 +11,7 @@ def get_historical_data():
     exchange = ccxt.binance()
     symbol = 'BTC/USDT'
     timeframe = '1h'
-    limit = 100  # historical data fetch points
+    limit = 100  # historical data fetch pts
     ohlcv = exchange.fetch_ohlcv(symbol, timeframe, limit)
     return ohlcv
 
@@ -25,47 +25,61 @@ def is_shooting_star(candle):
 
     return body_size <= body_size_threshold * candle[4] and upper_shadow >= upper_shadow_threshold * body_size
 
-def calculate_balance(current_balance, buy_price, sell_price, transaction_cost):
-    # balance
-    balance = current_balance * (1 - transaction_cost)
-    return balance * (sell_price / buy_price)
+def calculate_trade_amount(current_capital, price, transaction_cost, factor=-0.005):
+    max_trade_amount = current_capital * factor
+    trade_amount = max_trade_amount / price
+    transaction_fee = trade_amount * price * transaction_cost
+    return trade_amount - transaction_fee
 
 def trade_bitcoin(capital):
     percentage_change = 0.01  # 1%
-    shooting_star_threshold = -0.5
     transaction_cost = 0.0005
     current_capital = capital
+    bitcoin_amount = 0.0
+    cumulative_gain_loss = 0.0
+    current_gain_loss = 0.0
     hold = False
-
-    last_update_time = time.time()
 
     historical_data = get_historical_data()
 
     for candle in historical_data:
-        time.sleep(1)  # Real time behavior
+        time.sleep(1)  #real-time behavior
         close_price = get_real_time_bitcoin_price()
 
         price_change = (close_price / historical_data[0][4] - 1) * 100
 
         shooting_star = is_shooting_star(candle)
 
+        if hold:
+            current_gain_loss = (close_price - historical_data[0][4]) * bitcoin_amount
+            print(f"Current price:  {close_price} USD        Change: {price_change:.2f}%          Gain/Loss: {current_gain_loss:.2f} USD")
+
         if not hold and (price_change <= -percentage_change or shooting_star):
             hold = True
-            current_capital = current_capital * (1 - transaction_cost)
-            print(f"Buy: {close_price},     Change: {price_change:.2f}%,    Shooting Star: {shooting_star}")
+            bitcoin_amount_to_buy = calculate_trade_amount(current_capital, close_price, transaction_cost)
+            bitcoin_amount += bitcoin_amount_to_buy
+            usd_equivalent = bitcoin_amount_to_buy * close_price
+            current_capital -= usd_equivalent
+            print(f"Buy:            {usd_equivalent:.2f} BTC/USD     Change: {price_change:.2f}%          Gain/Loss: {current_gain_loss:.2f} USD")
+            if shooting_star:
+                print("SHOOTING STAR")
+            
+
         elif hold and (price_change >= percentage_change or shooting_star):
             hold = False
-            current_capital = calculate_balance(current_capital, buy_price=close_price, sell_price=close_price, transaction_cost=transaction_cost)
-            print(f"Sell: {close_price},    Change: +{price_change:.2f}%,  Shooting Star: {shooting_star}")
-        else:
-            print(f"On hold: {close_price},     Change: {price_change:.2f}%,    Shooting Star: {shooting_star}")
+            bitcoin_amount_to_sell = calculate_trade_amount(current_capital, close_price, transaction_cost)
+            bitcoin_amount -= bitcoin_amount_to_sell
+            usd_equivalent = bitcoin_amount_to_sell * close_price
+            current_capital += usd_equivalent
+            cumulative_gain_loss += current_gain_loss
+            print(f"Sell:          {usd_equivalent:.2f} BTC/USD    Change: +{price_change:.2f}%         Gain/Loss: {current_gain_loss:.2f} USD")
+            if shooting_star:
+                print("SHOOTING STAR")
 
-        current_time = time.time()
-        if hold and current_time - last_update_time >= 30:
-            print("")
-            print(f"Your bank acc.: {current_capital:.2f} USD")
-            print("")
-            last_update_time = current_time
+
+    print("Final Results:")
+    total_change = ((current_capital + cumulative_gain_loss) / capital - 1) * 100
+    print(f"Your bank acc.: {total_change:.2f} USD")
 
 if __name__ == "__main__":
     starting_capital = 100
